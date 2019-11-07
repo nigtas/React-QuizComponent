44a8314ef5deb21f0214fdc13fd19bf7679c5a6a
Vykintas Kumstys <Vykintas.Kumstys@bentley.com>
2019-11-07 15:43:17 +0200

    Stage 6

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -6,10 +6,14 @@ class QuizQuestionButton extends Component {
         this.quiz_question = { quiz_position: 1 }
     }
 
+    handleClick() {
+        this.props.clickHandler(this.props.button_text)
+    }
+
     render() {
         return (
             <li>
-                <button>{this.props.button_text}</button>
+                <button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
             </li>
         )
     }
