d990a80af95cdade6e315a2cb4496aa30299c0b3
Vykintas Kumstys <Vykintas.Kumstys@bentley.com>
2019-11-07 15:46:59 +0200

    Stage 7

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,12 +4,16 @@ import QuizQuestionButton  from './QuizQuestionButton.js'
 class QuizQuestion extends Component {
     constructor(props) {
         super(props)
-        this.quiz_question = { quiz_position: 1 }
+        this.state = { incorrectAnswer : false }
     }
 
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
+            this.setState({incorrectAnswer: false})
             this.props.showNextQuestionHandler()
+        }
+        else {
+            this.setState({incorrectAnswer: true})
         }
     }
 
@@ -26,6 +30,7 @@ class QuizQuestion extends Component {
                     })}
                 </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right.</p> : null}
             </main>
         )
     }
