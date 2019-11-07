1774a6a7f295160a41bd3fb653ba49516a663d3e
Vykintas Kumstys <Vykintas.Kumstys@bentley.com>
2019-11-07 12:16:35 +0200

    Stage 2

---
+++ src/QuizQuestion.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class QuizQueestion extends Component {
+    constructor(props) {
+        super(props)
+        this.quiz_question = { quiz_position: 1 }
+    }
+
+    render() {
+        return (
+            <main>
+                <section>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+                <ul>
+                    <li>
+                        {this.props.quiz_question.answer_options[0]}
+                    </li>
+                </ul>
+                </section>
+            </main>
+        )
+    }
+}
+
+export default QuizQueestion
